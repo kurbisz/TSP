deb781880235981d0b2de6ca1ad0f16ac78dea8f
krzys <krzysiu_urbisz@interia.pl>
2022-03-16 17:23:19 +0100

    Created objective function

--- src/main/java/org/example/algorithm/Algorithm.java
+++ src/main/java/org/example/algorithm/Algorithm.java
@@ -6,9 +6,18 @@ public abstract class Algorithm {
 
 	private TspData tspData;
 
+	protected int[] result;
+
 	public Algorithm(TspData tspData) {
 		this.tspData = tspData;
+		int size = tspData.getSize();
+		this.result = new int[size];
+		for(int i = 0; i < size; i++) {
+			result[i] = i;
+		}
 	}
+
+	public abstract void calculate();
 
 	private void drawPoint() {
 		// TODO
@@ -16,6 +25,14 @@ public abstract class Algorithm {
 
 	private void drawLine() {
 		// TODO
+	}
+
+	public int objectiveFunction() {
+		int sum = 0;
+		for(int i = 0; i < tspData.getSize() - 1; i++) {
+			sum += tspData.getDistance(result[i], result[i+1]);
+		}
+		return sum;
 	}
 
 }
---
+++ src/main/java/org/example/algorithm/SimpleAlgorithm.java
@@ -0,0 +1,17 @@
+package org.example.algorithm;
+
+import org.example.data.TspData;
+
+public class SimpleAlgorithm extends Algorithm {
+
+	public SimpleAlgorithm(TspData tspData) {
+		super(tspData);
+	}
+
+	@Override
+	public void calculate() {
+
+	}
+
+
+}
