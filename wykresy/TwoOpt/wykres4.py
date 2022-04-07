f85deee4f6a38dbfd5828c0b7b33d26683586ae2
krzys <krzysiu_urbisz@interia.pl>
2022-04-07 19:26:00 +0200

    wykresy

--- wykresy/TwoOpt/wykres4.py
+++ wykresy/TwoOpt/wykres4.py
@@ -21,7 +21,7 @@ def draw():
     r1 = np.arange(10)
     plt.title('Porownanie algorytmu 2-OPT w zaleznosci od rozwiazania poczatkowego')
     plt.bar(r1, to, color='green', width=l, label='Zwykly 2-OPT')
-    plt.bar(r1 + l, to, color='red', width=l, label='2-OPT dla losowego poczatku')
+    plt.bar(r1 + l, bto, color='red', width=l, label='2-OPT dla losowego poczatku')
     plt.xticks([r + l for r in range(10)], names)
     plt.legend()
     plt.xlabel('Rodzaj i wielkość problemu')
