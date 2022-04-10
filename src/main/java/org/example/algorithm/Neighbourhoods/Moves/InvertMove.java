6f6dfe98efd610189862579bfa72d8420009a519
Muciej <261745@student.pwr.edu.pl>
2022-04-10 17:06:37 +0200

    Tam z tym dodawaniem kolejnego ruchu w invert to zmieniłem, żeby po prostu nie traktował ruchu i j oraz j i tak samo dla asym

--- src/main/java/org/example/algorithm/Neighbourhoods/Moves/InvertMove.java
+++ src/main/java/org/example/algorithm/Neighbourhoods/Moves/InvertMove.java
@@ -3,16 +3,20 @@ package org.example.algorithm.Neighbourhoods.Moves;
 public class InvertMove implements Move {
     int from;
     int to;
-    public InvertMove(int from, int to) {
+    boolean symm;
+
+    public InvertMove(int from, int to, boolean symm) {
         this.from = from;
         this.to = to;
+        this.symm = symm;
     }
 
     @Override
     public boolean checkEqual(Move move) {
         if (move instanceof InvertMove) {
             InvertMove invertMove = (InvertMove) move;
-            return (invertMove.from == this.from && invertMove.to == this.to) || (invertMove.from == this.to && invertMove.to == this.from);
+            if (symm) return (from == invertMove.from && to == invertMove.to);
+            else return (invertMove.from == this.from && invertMove.to == this.to) || (invertMove.from == this.to && invertMove.to == this.from);
         }
         return false;
     }
