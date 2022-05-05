ce6b85259c4ec647fed67debd90e3e9441cb9fa1
Muciej <261745@student.pwr.edu.pl>
2022-05-05 16:32:15 +0200

    notatki z zajęć 8

--- src/main/java/org/example/RunClass.java
+++ src/main/java/org/example/RunClass.java
@@ -23,13 +23,13 @@ public class RunClass {
     public static FileLoader loader;
 
     public static void main(String[] args) {
-//        String file = "dane/d657.tsp";
+        String file = "dane/d657.tsp";
 //        String file = "dane/d198.tsp";
 //        String file = "dane/d1291.tsp";
 //        String file = "dane/pr299.tsp";
 //          String file = "kroB100.tsp";
 //        String file = "metro6.tsp";
-        String file = "metro7.tsp";
+//        String file = "metro7.tsp";
 //        String file = "metro8.tsp";
 //        String file = "rnd3.tsp";
         loaderTest(file);
@@ -67,7 +67,7 @@ public class RunClass {
         Result startingRes = nearestNeighbour.calculate();
 //        draw(startingRes);
 
-        TabooSearch2 ts = new TabooSearch2(data, startingRes, true, new BasicTabooList(7), new Invert(), new IterationsStop(10000), null, new Kick(100, 5), 1);
+        TabooSearch2 ts = new TabooSearch2(data, startingRes, true, new BasicTabooList(7), new Invert(), new IterationsStop(10000), null, new Kick(100, 5), 6);
         Result endRes = ts.calculate();
         System.out.println("At the beginning: " + startingRes.calcObjectiveFunction());
         System.out.println("At the end: " + endRes.calcObjectiveFunction());
